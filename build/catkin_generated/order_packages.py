687c7c964eca79675a3a1d8807733fa9c0c147d9
ViniciusFelismino8 <felisminovinicius8@gmail.com>
2020-03-20 18:35:27 -0300

    world in gazebo

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/vinicius/timon_hm_manipulator/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/vinicius/timon_hm_manipulator/devel;/opt/ros/melodic".split(';') if "/home/vinicius/timon_hm_manipulator/devel;/opt/ros/melodic" != "" else []
--- build/table_challenge/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vinicius/timon_hm_manipulator/src/table_challenge
-# Build directory: /home/vinicius/timon_hm_manipulator/build/table_challenge
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
